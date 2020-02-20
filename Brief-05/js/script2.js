e76c0e02c83e9b3e49a00ddbf5af538f63a71e8e
Fattati <57252669+Fattati@users.noreply.github.com>
2020-02-20 12:11:46 +0100

    Car rentals Website

---
+++ Brief-05/js/script2.js
@@ -0,0 +1,43 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+// popup
+
+function openForm() {
+	document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+	document.getElementById("myForm").style.display = "none";
+  }
+
+//   Validation Email
+function nameValidation(email){
+	var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+	return pattern.test(email);
+  }
+  function validation(){
+	var email = document.getElementById("input1").value;
+	if(email == ""){
+	  alert("Entre Your Email");
+	}
+	else{
+	  if(nameValidation(email))
+	  {
+		alert("Done");
+	  }
+	  else {
+		alert("Email is not Valid");
+	  }
+	}
+  
+  } 
